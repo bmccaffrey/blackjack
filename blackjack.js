191b808c8694fcb192e2790ef44167ab0241a059
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-04-08 18:09:15 -0500

    add method to generate unicode for each suit-rank pair

--- blackjack.js
+++ blackjack.js
@@ -13,3 +13,32 @@ Card.prototype.value = function() {
     return 13;
   } else { return this.rank; }
 };
+
+// 26 LOC
+Card.prototype.unicode = function() {
+  let uni = '&#x1F0';
+  if(this.suit === "Spades") {
+    uni += 'A';
+  } else if(this.suit === "Hearts") {
+    uni += 'B';
+  } else if(this.suit === "Diamonds") {
+    uni += 'C';
+  } else {
+    uni += 'D';
+  }
+
+  if(this.rank === 10) {
+    uni += 'A';
+  } else if(this.rank === "Jack") {
+    uni += 'B';
+  } else if(this.rank === "Queen") {
+    uni += 'D';
+  } else if(this.rank === "King") {
+    uni += 'E';
+  } else if(this.rank === "Ace") {
+    uni += 1;
+  } else {
+    uni += this.rank;
+  }
+  return uni;
+}
