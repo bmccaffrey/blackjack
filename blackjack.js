2752d044700427c6f073ebc7e06333168424e33b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-04-08 18:01:17 -0500

    add method to return integer for rank of Card

--- blackjack.js
+++ blackjack.js
@@ -3,3 +3,13 @@ function Card(suit, rank) {
   this.suit = suit;
   this.rank = rank;
 }
+
+// 8 LOC
+Card.prototype.value = function() {
+  var value;
+  if (this.rank === 'Jack' || this.rank === 'Queen' || this.rank === 'King') {
+    return 10;
+  } else if (this.rank === 'Ace') {
+    return 13;
+  } else { return this.rank; }
+};
